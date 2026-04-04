757156f6f18ff94565dbb85bff83adb2041440df
HahsTag <hashtag2829@gmail.com>
2026-04-04 18:55:21 -0500

    se han agregado los modulos del programa

---
+++ menuUsuario.py
@@ -0,0 +1,16 @@
+
+eleccion = input("Digite la eleccion \n (1) Si desea Ingresar un usuario nuevo \n (2) Si desesa ver cuantos usuarios hay registrados \n (3) Si desea actualizar la informacion de un usuario ya existente \n (4) Si desea Borrar a un usuario \n ingresa tu opcion: ")
+
+match eleccion:
+    case 1:
+        print("---------REGISTRAR A UN USUARIO---------")
+        nombre = print("Ingrese el nombre del usuario nuevo: ")
+        apellido = print("Ingrese el apellido del usuario nuevo: ")
+        correo = print("Ingrese el correo del usuario nuevo: ")
+        fecha_nacimiento = print("Ingrese la fecha de naciemiento del usuario nuevo: ")
+
+    case 2: 
+        print ("")
+        
+
+
