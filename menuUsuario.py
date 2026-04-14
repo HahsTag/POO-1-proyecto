4df9b5aac28f9c310ce97dd503358ea916354776
HahsTag <hashtag2829@gmail.com>
2026-04-13 22:48:45 -0500

    Modelo de usuarios terminado

--- menuUsuario.py
+++ menuUsuario.py
@@ -18,6 +18,7 @@ class Usuario:
 
         for clave in listaUsuarios.keys():
             if clave == self.cedula:
+                print("Ese numero de cedula ya existe")
                 self.isCeV == True
                 return
 
@@ -33,6 +34,7 @@ class Usuario:
     def verUsuarios(self):
         if listaUsuarios:
             df_usuarios = pd.DataFrame(listaUsuarios)
+            df_usuarios = df_usuarios.T
             print(df_usuarios)
         else:
             print("No se han registrado usuarios")           
@@ -96,31 +98,38 @@ class Usuario:
                     del listaUsuarios[clave]
 
 eleccion = 0
+def validarCorreo():
+    
+    global correo
+    i = 0
+    k = 0 
+    while i != 1:
+        correo = input("Ingrese el correo del usuario nuevo: ")
+        for arrova in correo:
+                        if arrova == " ":
+                            break
+                        elif arrova == "@":
+                            correo = correo
+                        else: 
+                            k += 1
+                             
+        if k == len(correo):
+            print("Recuerde que el correo NO permite caracteres especiales y debe tener contener un arrova")
+        else:
+            i = 1
+    return correo
 
 while eleccion != 5:
     eleccion = int(input("Digite la eleccion \n (1) Si desea Ingresar un usuario nuevo \n (2) Si desesa ver cuantos usuarios hay registrados \n (3) Si desea actualizar la informacion de un usuario ya existente \n (4) Si desea Borrar a un usuario \n (5) salir \n ingresa tu opcion: "))
     match eleccion:
-        case 1:
-            k = 0  
+        case 1: 
             print("---------REGISTRAR A UN USUARIO---------")
             nombre = input("Ingrese el nombre del usuario nuevo: ")
             apellido = input("Ingrese el apellido del usuario nuevo: ")
-            correoNuevo = input("Ingrese el correo del usuario nuevo: ")
-            for arrova in correoNuevo:
-                if arrova == " ":
-                    k = len(correoNuevo)
-                    break
-                elif arrova == "@":
-                    correo = correoNuevo
-                else:
-                    k += 1
-            if len(correoNuevo) == k:
-                print("Recuerde que el correo NO permite caracteres especiales y debe tener contener un arrova")
-                break
-             
+            validarCorreo()
             fechaN = input("Ingrese la fecha de nacimiento del usuario nuevo: ")
-            cedula = int(input("Ingrese la cedula del usuario nuevo: "))
             contra = input("Ingrese la contraseña del usuario nuevo: ")
+            cedula = int(input("Ingrese la cedula del usuario nuevo: "))
             usu1 = Usuario(nombre, apellido, correo, fechaN, cedula, contra)
             usu1.registroUsuario()
 
